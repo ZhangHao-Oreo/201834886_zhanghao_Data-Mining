d51e5486c759e50f458b753cdb005fcecf971aca
zhanghao <zh4869zh@gmail.com>
2018-10-25 22:27:54 +0800

    2018_10_25_(commit_10)
    划分数据集和测试集90%训练集10%测试集

--- Homework_1/vsm/init_set.py
+++ Homework_1/vsm/init_set.py
@@ -12,7 +12,7 @@ Created on Tue Oct 16 21:40:34 2018
 
 #遍历每一个文件夹下文件
 import os
-
+import shutil  #用于拷贝文件
 #-----other-----
 dirs_path = ("E:\\new1")
 """
@@ -39,3 +39,35 @@ def travel_all_file(dirs_path):
         print (filename)
         i=i+1
     return i
+
+
+
+
+
+"""
+name:
+input:dirs_path  [str]   所有数据根目录
+划分数据集
+"""
+def init_set(dirs_path='E:\\20news-18828'):
+    for dirs in os.listdir(dirs_path):
+        files_path = os.path.join(dirs_path, dirs)
+        os.makedirs('E:\\train_set\\' + dirs)
+        os.makedirs('E:\\test_set\\' + dirs)
+        i = 0
+        for file in os.listdir(files_path):
+            if i < len(os.listdir(files_path)) * 0.9:
+                train_file = os.path.join('E:\\train_set\\' + dirs, file)
+                shutil.copyfile(os.path.join(files_path, file),train_file)
+            else:
+                test_file = os.path.join('E:\\test_set\\' + dirs, file)
+                shutil.copyfile(os.path.join(files_path, file), test_file)
+            i += 1
+    
+    
+    
+    
+    
+    
+    
+    

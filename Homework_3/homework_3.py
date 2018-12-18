0391fb3ebbaae0423ad6bbd91e36bb280c3d7b27
ZhangHao-Oreo <zh4869zh@gamil.com>
2018-12-18 15:00:47 +0800

    2018_12_18_(commit_4)
    调通Kmeans

--- Homework_3/homework_3.py
+++ Homework_3/homework_3.py
@@ -13,6 +13,10 @@ import string
 from nltk.corpus import stopwords     #使用nltk提供的的stopwords
 from nltk.stem.porter import PorterStemmer        # 提取词干
 import math
+import re
+import numpy as np
+import time
+from sklearn.cluster import KMeans
 
 
 dirs_path = "E:\\Tweets.txt"
@@ -25,37 +29,39 @@ name:read_text
 读取信息
 """
 def read_text(dirs_path):
-​    vectors = []
-​    label = []
-​    f = open(dirs_path, 'r')
-​    line = f.readline()
-​    while line:        
-​        label.append(int(line[line.rfind("\"cluster\"")+11:line.rfind("}")]))
-​        vectors.append(line[line.find("\"text\"")+9:line.rfind(",")-1])
-​        line = f.readline()
-​    return vectors, label
+    vectors = [] 
+    label = []
+    f = open(dirs_path, 'r')
+    line = f.readline()
+    while line: 
+        label.append(int(line[line.rfind("\"cluster\"")+11:line.rfind("}")]))
+        vectors.append(line[line.find("\"text\"")+9:line.rfind(",")-1])
+        line = f.readline()
+    return vectors, label
+    
+    
+   
 
 
 texts,label =  read_text( "E:\\Tweets.txt")
 
 
 
-​     
 
 """
 name:read_text
 读取信息
 """
 def read_text(dirs_path):
-​    vectors = []
-​    label = []
-​    f = open(dirs_path, 'r')
-​    line = f.readline()
-​    while line:        
-​        label.append(int(line[line.rfind("\"cluster\"")+11:line.rfind("}")]))
-​        vectors.append(line[line.find("\"text\"")+9:line.rfind(",")-1])
-​        line = f.readline()
-​    return vectors, label
+    vectors = []
+    label = []
+    f = open(dirs_path, 'r')
+    line = f.readline()
+    while line:        
+        label.append(int(line[line.rfind("\"cluster\"")+11:line.rfind("}")]))
+        vectors.append(line[line.find("\"text\"")+9:line.rfind(",")-1])
+        line = f.readline()
+    return vectors, label
 
 """
 name:get_tokens
@@ -64,12 +70,11 @@ input:text [str]
 output:tokens [list]
 """
 def get_tokens(text):
-​    lowers = text.lower()   #大小写
-​                            #去除标点符号
-​    remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
-​    no_punctuation = lowers.translate(remove_punctuation_map)
-​    tokens = nltk.word_tokenize(no_punctuation)
-​    return tokens
+    lowers = text.lower()   #大小写
+    remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
+    no_punctuation = lowers.translate(remove_punctuation_map)
+    tokens = nltk.word_tokenize(no_punctuation)
+    return tokens
 
 
 """
@@ -79,10 +84,10 @@ input:tokens [list]
 output:stemmed [list]
 """
 def stem_tokens(tokens,Stemmer):
-​    stemmed = []
-​    for item in tokens:
-​        stemmed.append(Stemmer.stem(item))
-​    return stemmed
+    stemmed = []
+    for item in tokens:
+        stemmed.append(Stemmer.stem(item))
+    return stemmed
 
 
 
@@ -97,22 +102,22 @@ output:filtered [list]
 
 
 def remove_stopwords(tokens):
-​    filtered = [w for w in tokens if not w in stopwords.words('english')]   #过滤stopwords
-​    return filtered
+    filtered = [w for w in tokens if not w in stopwords.words('english')]   #过滤stopwords
+    return filtered
 
 
 def process_text(text_):
-​    vec_tmp = []
-​    for text in text_:
-​        tokens = get_tokens(text)
-​        stemmed = stem_tokens(tokens, PorterStemmer() )
-​        text = remove_stopwords(stemmed)
-​        vec_tmp.append(text)
-​    return vec_tmp
+    vec_tmp = []
+    for text in text_:
+        tokens = get_tokens(text)
+        stemmed = stem_tokens(tokens, PorterStemmer() )
+        text = remove_stopwords(stemmed)
+        vec_tmp.append(text)
+    return vec_tmp
 
 
 def hasNumbers(inputString):
-​    return bool(re.search(r'\d', inputString)) 
+    return bool(re.search(r'\d', inputString)) 
 """
 name:build_dict
 input:text [str] 输入单个文本全部内容  texts [list]   所有文档
@@ -120,33 +125,31 @@ input:text [str] 输入单个文本全部内容  texts [list]   所有文档
 output:dict_tmp [str] 
 """
 def build_dict(texts_):
-​    dict_tmp = []
-​    #words_list = str.split(text)
-​    for text in texts_:      
-​        words_list = text
-​        for words in words_list:    
-​            if hasNumbers(words) == False :
-​                #        print ( hasNumbers(words))
-​                #    for w in words:
-​                length_words = len(words)
-​                if length_words > 2 and length_words < 17:
-​                    #if text.count(words) > 3:
-​                    #    if word_in_file_num(texts,words) > 5:
-​                    if words not in dict_tmp:
-​                        dict_tmp.append(words)
-​                        #print (words)
-​    return dict_tmp
+    dict_tmp = []
+    #words_list = str.split(text)
+    for text in texts_:      
+        words_list = text
+        for words in words_list:    
+            if hasNumbers(words) == False :
+                #        print ( hasNumbers(words))
+                #    for w in words:
+                length_words = len(words)
+                if length_words > 2 and length_words < 17:
+                    #if text.count(words) > 3:
+                    #    if word_in_file_num(texts,words) > 5:
+                    if words not in dict_tmp:
+                        dict_tmp.append(words)
+                        #print (words)
+    return dict_tmp
 
-
-​    
 
 def compute_tf(text,word):
-​    tf = text.count(word)
-​    if tf > 0:
-​       tf = 1 + math.log(tf)
-​    else:
-​       tf = 0
-​    return tf
+    tf = text.count(word)
+    if tf > 0:
+        tf = 1 + math.log(tf)
+    else:
+        tf = 0
+    return tf
 
 
 """
@@ -156,21 +159,21 @@ input:texts  [list]  所有文本  ,word   [str]  要查的词
 output:num  [int] 
 """
 def word_in_file_num(texts,word):
-​    num = 0
-​    for text in texts:
-​        if text.count(word) >= 1:
-​            num += 1
-​    return num
+    num = 0
+    for text in texts:
+        if text.count(word) >= 1:
+            num += 1
+    return num
 
 
 def compute_idf(texts,word,Dict_full):
-​    idf = math.log(len(texts) / Dict_full[word])
-​    return idf
+    idf = math.log(len(texts) / Dict_full[word])
+    return idf
 
 
 
 def compute_df(texts,Dict):
-​    Dict_full = dict()
+    Dict_full = dict()
 #    i = 0
     for word in Dict:
 #        print ("word_tf " ,i)
@@ -182,12 +185,32 @@ def compute_df(texts,Dict):
 
 
 
+def compute_tf_idf(Dict,texts,Dict_full):
+    print ("计算TF-IDF")
+    star = time.time()
+    vectors = []
+    for text in texts:
+        vector = []
+        for word in Dict:
+            tf = compute_tf(text,word)
+            idf = Dict_full[word]
+            tf_idf = tf * idf
+            #tf_idf = int(tf_idf+0.5)
+            vector.append(tf_idf)
+        vectors.append(vector)
+    end = time.time()
+    print (end-star)
+    print ("计算TF-IDF____OK")
 
-texts,label =  read_text( "E:\\Tweets.txt")
-text_1 = process_text(text)
-dict_1 = build_dict(text_1)      
+    return vectors
+
+
+texts,label =  read_text( "D:\\Tweets.txt")
+text_1 = process_text(texts)
+dict_1 = build_dict(text_1)  
 Dict_full = compute_df(text_1,dict_1)
 
+vec = compute_tf_idf(dict_1,text_1,Dict_full)
 
 X = np.array(vec, dtype=float)
 y = np.array(label)
@@ -199,11 +222,22 @@ k = len(set(label))
 name:kmeans_my
 K-Means
 """
-def kmeans_my(k,X):
+def kmeans_my(k,X,y):
     km = KMeans(n_clusters=k).fit(X)
+    tmp = km.labels_
     pr = km.predict(X)
+
+    for i in tmp:
+        if pr[it_] ==
     return pr
 
 
-
+    it_ = 0
+    count = 0
+    for i in tmp:
+        if i == y[it_]：:
+            count += 1
+    
+    
+pr = kmeans_my(k,X,y)
 
